907dccb780b49fe79f03260df894e9e34b0bbbe5
ahmadaliasghar <ahmadali2882@yahoo.com>
2023-02-01 12:12:24 +0500

    Initial commit

---
+++ MSWord/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MSWord
+{
+    public partial class Form1 : Form
+    {
+        string fileName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Ahmad Ali Word";
+            saveFileDialog1.DefaultExt = ".rtf"; //docx
+            saveFileDialog1.CheckFileExists = true;
+            if(saveFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                richTextBox1.SaveFile(saveFileDialog1.FileName);
+                fileName = saveFileDialog1.FileName;
+            }
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Open File -> Ahmad Ali Word";
+            openFileDialog1.InitialDirectory = "Desktop";
+            openFileDialog1.Filter = "Rich Text Format|*rtf|All Files|*.*";
+
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                richTextBox1.LoadFile(openFileDialog1.FileName);
+                fileName = openFileDialog1.FileName;
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SaveFile(fileName);
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(fontDialog1.ShowDialog()==DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fontDialog1.Font;
+            }
+
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(colorDialog1.ShowDialog()==DialogResult.OK)
+            {
+                richTextBox1.SelectionColor = colorDialog1.Color;
+            }
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo();
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Redo();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(printDialog1.ShowDialog()==DialogResult.OK)
+            {
+              
+            }
+        }
+    }
+}
